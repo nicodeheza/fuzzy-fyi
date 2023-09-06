d280e764a3b8d4cdea520873f22f8d224b3485de
nicodeheza <nicolasdeheza@hotmail.com>
2023-09-06 11:44:19 -0300

    add gitHub button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,52 @@
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import {Auth} from '@supabase/auth-ui-react'
+import {ThemeSupa} from '@supabase/auth-ui-shared'
+import {useSession, useSupabaseClient} from '@supabase/auth-helpers-react'
+import {useRouter} from 'next/router'
+import {useEffect, useState} from 'react'
 
 const Home = () => {
-  const router = useRouter();
-  const session = useSession();
-  const supabase = useSupabaseClient();
-  const [display, setDisplay] = useState("none");
+	const router = useRouter()
+	const session = useSession()
+	const supabase = useSupabaseClient()
+	const [display, setDisplay] = useState('none')
 
-  useEffect(() => {
-    let ts = setTimeout(() => setDisplay(""));
+	useEffect(() => {
+		let ts = setTimeout(() => setDisplay(''))
 
-    return () => clearTimeout(ts);
-  }, []);
+		return () => clearTimeout(ts)
+	}, [])
 
-  if (session) {
-    router.push("/dashboard/jobs");
-  }
+	if (session) {
+		router.push('/dashboard/jobs')
+	}
 
-  return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <div style={{ width: "320px", display }}>
-          {!session ? (
-            <Auth
-              supabaseClient={supabase}
-              appearance={{ theme: ThemeSupa }}
-              theme="dark"
-              providers={[]}
-            />
-          ) : (
-            <div style={{ textAlign: "center" }}>
-              <span>Welcome {session.user.email}</span>
-            </div>
-          )}
-        </div>
-      </div>
-    </>
-  );
-};
+	return (
+		<>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '100vh'
+				}}
+			>
+				<div style={{width: '320px', display}}>
+					{!session ? (
+						<Auth
+							supabaseClient={supabase}
+							appearance={{theme: ThemeSupa}}
+							theme="dark"
+							providers={['github']}
+						/>
+					) : (
+						<div style={{textAlign: 'center'}}>
+							<span>Welcome {session.user.email}</span>
+						</div>
+					)}
+				</div>
+			</div>
+		</>
+	)
+}
 
-export default Home;
+export default Home
