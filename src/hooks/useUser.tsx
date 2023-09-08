b3539b69b7a02a01afe1972b568bc5ff62c6fec1
nicodeheza <nicolasdeheza@hotmail.com>
2023-09-08 12:44:43 -0300

    api call

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,12 +1,10 @@
-import {User} from '@prisma/client'
 import {supabase} from '@services/supabase'
 import {useState} from 'react'
-
-type NewUser = Omit<User, 'id' | 'createdAt' | 'updatedAt'>
 
 function useUser() {
 	const [isLoading, setIsLoading] = useState(false)
 
+	// check this
 	const getUser = async (userId: string) => {
 		setIsLoading(true)
 		const {data, error} = await supabase
@@ -21,27 +19,36 @@ function useUser() {
 		return data
 	}
 
-	const createUser = async (newUser: NewUser) => {
+	const crateAccount = async (organizationName: string) => {
 		setIsLoading(true)
-		const {error, data} = await supabase.from('users').insert(newUser)
-		setIsLoading(false)
-		if (error) throw error
-		return data
-	}
+		const {
+			data: {session}
+		} = await supabase.auth.getSession()
+		if (!session) throw new Error('missing session')
+		try {
+			const response = await fetch('/api/account', {
+				method: 'POST',
+				body: JSON.stringify({organizationName}),
+				headers: {
+					'Content-Type': 'application/json',
+					authorization: `Bearer ${session.access_token}`
+				}
+			})
+			const {apiKey} = await response.json()
 
-	const updateUser = async (authId: string, user: Partial<User>) => {
-		setIsLoading(true)
-		const {error, data} = await supabase.from('users').update(user).eq('authId', authId)
-		setIsLoading(false)
-		if (error) throw error
-		return data
+			setIsLoading(false)
+
+			return apiKey
+		} catch (error) {
+			setIsLoading(false)
+			throw error
+		}
 	}
 
 	return {
 		isLoading,
 		getUser,
-		createUser,
-		updateUser
+		crateAccount
 	}
 }
 
